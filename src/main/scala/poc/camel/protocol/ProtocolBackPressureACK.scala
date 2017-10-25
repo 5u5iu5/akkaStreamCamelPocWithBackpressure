a6166e0bc16bf537676227834ad8d0d11de758f3
sucius <alvaro.pozo.cuenca@gmail.com>
2017-10-25 08:52:37 +0200

    Added Kafka features to consume

--- src/main/scala/poc/camel/protocol/ProtocolBackPressureACK.scala
+++
@@ -1,8 +0,0 @@
-package poc.camel.protocol
-
-sealed trait ProtocolBackPressureACK
-
-case object ACK extends ProtocolBackPressureACK
-case object INITMESSAGE extends ProtocolBackPressureACK
-case object ONCOMPLETE extends ProtocolBackPressureACK
-
